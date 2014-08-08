b2711ce78cdd20d3d34cc157b79c4ce00a7780ad
Ben Powell <blpowell@gmail.com>
2014-08-08 19:03:05 +0100

    Fixed issue with events < 24 hrs away

--- lib/date_helper.rb
+++ lib/date_helper.rb
@@ -1,8 +1,13 @@
 module DateHelper
 	def self.readable_future_date(date)
-		#days_away = (date - DateTime.now).to_i #TODO: Returns 0 some times
 		days_away = ((date - Time.zone.now ) / 1.day).to_i
+		hrs_away = ((date - Time.zone.now ) / 1.hours).to_i
 		case days_away
+		when 0
+			if hrs_away > date.hour
+				"on tomorrow"
+			end			
+
 		when 1
   			"on tomorrow"
 		when 2
