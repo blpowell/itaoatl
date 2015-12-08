a5106f4dfdff9d4e1d39248f0df828945d0c85bd
Ben Powell <blpowell@gmail.com>
2015-12-08 07:59:22 +0000

    Fixed issue with dates

--- lib/date_helper.rb
+++ lib/date_helper.rb
@@ -1,12 +1,14 @@
 module DateHelper
 	def self.readable_future_date(date)
-		days_away = ((date - Time.zone.now ) / 1.day).to_i
-		hrs_away = ((date - Time.zone.now ) / 1.hours).to_i
+
+		days_away = (date - Date.today).to_i
+		hrs_away =  ((date - Date.today ) / 1.hours).to_i
+
 		case days_away
 		when 0
 			if hrs_away > date.hour
 				"on tomorrow"
-			end			
+			end
 
 		when 1
   			"on tomorrow"
